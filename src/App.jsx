181d789c8fd7b9216ea0a7daef8180c596b5b32f
Zura Doghanadze <zuradoghanadze@gmail.com>
2023-10-01 10:45:56 +0400

    add responsive disaign

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import batonsIcon from "../src/assets/img/batons icon.png";
 function App() {
   return (
     <div className="flex bg-blue-950 w-full h-full justify-center items-center">
-      <div className="bg-card-baclground flex flex-col max-w-xl w-full rounded-xl relative">
-        <div className="flex items-center flex-col my-10 mx-10">
+      <div className="bg-card-baclground flex flex-col md:max-w-xl max-w-sm w-full rounded-xl relative md:mx-0 mx-64">
+        <div className="flex items-center flex-col my-14 mx-10">
           <h1 className="mb-8 text-lg tracking-wider font-bold uppercase text-h1-color 		">
             advice
           </h1>
@@ -17,14 +17,18 @@ function App() {
             Assumenda"
           </p>
           <div className="flex gap-4 items-center">
-            <div className="w-48 h-1 bg-line-background"></div>
+            <div className="md:w-48 w-32 h-1 bg-line-background"></div>
             <img src={pauseIcon} alt="icon" />
-            <div className="w-48 h-1 bg-line-background"></div>
+            <div className="md:w-48 w-32 h-1 bg-line-background"></div>
           </div>
         </div>
-        <div className="flex items-center justify-center absolute left-2/4 w-16 h-16 rounded-full bg-btn-background">
+        <div
+          className="flex items-center justify-center absolute left-1/2 transform -translate-x-1/2 
+                    inset-92 w-16 h-16 rounded-full bg-btn-background cursor-pointer 
+                    transition-transform hover:scale-105 hover:shadow-custom-shadow"
+        >
           <button>
-            <img src={batonsIcon} alt="" />
+            <img src={batonsIcon} alt="img" />
           </button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ export default {
       colors: {
         "h1-color": "#53FFAA",
       },
+      inset: {
+        92: "92%",
+      },
+      boxShadow: {
+        "custom-shadow": "0px 0px 40px 0px #53FFAA",
+      },
     },
   },
   plugins: [],
